8a809ef14ad1cf0546dd2db2ba5689cdb0c29c8c
Mateusz <holowieszko.m@gmail.com>
2019-03-04 17:54:31 +0100

    Added new material design modules

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -4,13 +4,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 @NgModule({
   exports: [
     MatToolbarModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule
   ]
 })
 
