dcfbf0355f86e3c11db83f7e5079575071606ed0
Mateusz <holowieszko.m@gmail.com>
2019-03-04 13:10:45 +0100

    Added routing, md & reactive forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { AngularMaterialModule } from '../angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule,
+    AngularMaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
