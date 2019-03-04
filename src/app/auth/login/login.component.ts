084296e28e6e5fce1b346797ed4d25136c0d2479
Mateusz <holowieszko.m@gmail.com>
2019-03-04 15:41:47 +0100

    Modified login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private readonly authService: AuthService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      username: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      password: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  onLogin() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const data = {
+      username: this.form.value.username,
+      password: this.form.value.password
+    };
+
+    this.authService.login(data);
   }
 
 }
