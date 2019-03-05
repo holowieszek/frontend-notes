fb2e352fa53623c33b673ef64bc57d82f24df687
Mateusz <holowieszko.m@gmail.com>
2019-03-05 15:31:59 +0100

    Added routing for creating notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { NotesListComponent } from './notes/notes-list/notes-list.component';
+import { NotesCreateComponent } from './notes/notes-create/notes-create.component';
 
 const routes: Routes = [
   { path: 'notes', component: NotesListComponent, canActivate: [AuthGuard] },
+  { path: 'notes/create', component: NotesCreateComponent, canActivate: [AuthGuard] },
   { path: '', loadChildren: './auth/auth.module#AuthModule' },
 ];
 
